17444dc025a3ea7e9ef6d7b78f14d0512b6fa7a1
Daniel Colman <danielcolman@outlook.com>
2022-03-15 11:27:07 -0300

    Agrando variable a validacionViewController

--- RoshKaru/ValidacionViewController.swift
+++ RoshKaru/ValidacionViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ValidacionViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var jsonVcLogin:String?
+        override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
