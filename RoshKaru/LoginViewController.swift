e12fdefc564711f76cdbebd89bed83d29a3228e7
Enrique Pitta <bootcamp@ROSHKA-MACMINI-01.local>
2022-03-15 15:26:32 -0300

    Enviar accestoken y challenge al sgte viewcontroller

--- RoshKaru/LoginViewController.swift
+++ RoshKaru/LoginViewController.swift
@@ -17,28 +17,32 @@ import MaterialComponents.MaterialTextControls_OutlinedTextFields
 
 class LoginViewController: UIViewController {
     var json:String?
+    
+    var accessToken:String?
+    var challenge:String?
     //@IBOutlet weak var NumeroCelular: UITextField!
     @IBOutlet weak var BtnSendNumber: UIButton!
     
     @IBOutlet weak var StackLogin: UIStackView!
     let NumeroCelular = MDCOutlinedTextField()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         BtnSendNumber.addTarget(self, action: #selector(SendNumber(_:)), for: .touchUpInside)
         
-        //let estimatedFrame = CGRect(x: 0, y: 0, width: 0, height: 0)
-       // let NumeroCelular = MDCOutlinedTextField(frame: estimatedFrame)
+        
         NumeroCelular.label.text = "Phone number"
         NumeroCelular.placeholder = "Ej: 09Xx-XXX-XXX"
-        NumeroCelular.leadingAssistiveLabel.text = "This is helper text"
+        //NumeroCelular.leadingAssistiveLabel.text = "This is helper text"
         NumeroCelular.sizeToFit()
-        NumeroCelular.setOutlineColor(.systemBlue, for: .normal)
-        NumeroCelular.setOutlineColor(.blue, for: .editing)
+        NumeroCelular.setOutlineColor(.systemPurple, for: .normal)
+        NumeroCelular.setOutlineColor(.purple, for: .editing)
         
-        //view.addSubview(textField)
         StackLogin.addArrangedSubview(NumeroCelular)
         StackLogin.addArrangedSubview(BtnSendNumber)
+        
+        
+        
     }
     
     @IBAction func SendNumber(_ sender: Any) {
@@ -69,7 +73,17 @@ class LoginViewController: UIViewController {
         
     }
     
-    func start_login(phoneNumber: String, accessToken:String? ) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier ==  "IDverificacion"{
+            if let nexviewcontroller  = segue.destination as? ValidacionViewController {
+                nexviewcontroller.accessToken = self.accessToken
+                nexviewcontroller.challenge = self.challenge
+            }
+        }
+    }
+
+
+    private func start_login(phoneNumber: String, accessToken:String? ) {
         let BASEURL = "https://texo.thebirdmaker.com/eat"
         var urlComponents = URLComponents(string: "\(BASEURL)/start_login")!
         var queryItems: [URLQueryItem] = [
@@ -83,33 +97,93 @@ class LoginViewController: UIViewController {
         print(url.absoluteString)
         let request = URLRequest(url: url)
         URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                self.hud?.hide(animated: true)
+            }
             if let error = error {
                 print(error);
             } else if let data = data {
+                let httpResponse = response as? HTTPURLResponse
+                let status = httpResponse?.statusCode ?? 0
+                let statusCodeIsError = status > 299 || status < 200
                 
-                let json = try! JSONSerialization.jsonObject(with: data, options: [.fragmentsAllowed])
+                printDebugJson(data)
                 
-                let jsonString = String(data: try! JSONSerialization.data(withJSONObject: json, options: [.prettyPrinted]), encoding: .utf8)!
-                
-                print("\(String(data: try! JSONSerialization.data(withJSONObject: json, options: [.prettyPrinted]), encoding: .utf8)!)")
-                
-                self.json = String(jsonString)
-                DispatchQueue.main.async {
-                    self.performSegue(withIdentifier: "IDverificacion", sender: self.json)
+                if let ret: StartLoginRet = DecodableFromJson(data) {
+                    
+                    // guardar el access token en defaults
+                    UserDefaults.standard.setValue(ret.session.accessToken, forKey: "accessToken")
+                    UserDefaults.standard.synchronize()
+                    
+                    DispatchQueue.main.async {
+                        // ir al segue de verificacion
+                        self.accessToken = ret.session.accessToken
+                        self.challenge = ret.login.challenge
+                        self.performSegue(withIdentifier: "IDverificacion", sender: nil)
+                    }
+
+                } else if let error: ErrorRet = DecodableFromJson(data) {
+                    DispatchQueue.main.async {
+                        // mostrar error
+                        self.showError(error.userMsg ?? error.msg ?? "Ocurrió un error!")
+                    }
+                } else if statusCodeIsError {
+                    assert(false)
                 }
+                
             }
-            
         }.resume()
+        
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier ==  "IDverificacion"{
-            if let nexviewcontroller  = segue.destination as? ValidacionViewController {
-                nexviewcontroller.jsonVcLogin = self.json
-            }
-        }
+
+}
+
+struct ErrorRet: Codable {
+    let errorCode: String?
+    let userMsg: String?
+    let msg: String?
+    let actions: String?
+}
+
+func GetAccessToken() -> String? {
+    UserDefaults.standard.value(forKey: "accessToken") as? String
+}
+
+func printDebugJson(_ jsonData: Data ) {
+    if let json = try? JSONSerialization.jsonObject(with: jsonData, options: [.fragmentsAllowed]),
+       let jsonData = try? JSONSerialization.data(withJSONObject: json, options: [.prettyPrinted]),
+       let str = String(data: jsonData, encoding: .utf8) {
+        print(str)
     }
 }
+
+func DecodableFromJson<T: Decodable>(_ data: Data) -> T? {
+    return try? JSONDecoder().decode(T.self, from: data)
+}
+
+struct StartLoginRet: Codable {
+    struct Session: Codable {
+    
+        let key1: String // " : "ee5874fb-706e-4cfc-8afb-33b6d4067418",
+        let new: Bool // " : true,
+        let accessToken: String // " : "ee5874fb-706e-4cfc-8afb-33b6d4067418",
+        let key2: String? // " : null,
+        let table: String // " : "session",
+        let userKey: String? // " : null
+        
+    }
+    struct Login: Codable {
+        let challenge: String // " : "011c945f30ce2cbafc452f39840f025693339c42",
+        let key1: String // " : "ee5874fb-706e-4cfc-8afb-33b6d4067418",
+        let key2: String? // " : null,
+        let table: String // " : "login1",
+        let phoneNumber: String // " : "+595971305003"
+    }
+    let session: Session
+    let login: Login
+    
+}
+
 extension String {
  
     var trimmed: String {
