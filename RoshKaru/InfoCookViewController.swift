78743d0c01413555a784deb4252bc5262eb5bba2
Arturo Fleitas <arturofleitasc@gmail.com>
2022-03-18 16:55:45 -0300

    launch screen e icono de la app

---
+++ RoshKaru/InfoCookViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InfoCookViewController.swift
+//  RoshKaru
+//
+//  Created by Bootcamp on 3/18/22.
+//  Copyright © 2022 Bootcamp. All rights reserved.
+//
+
+import UIKit
+
+class InfoCookViewController: UIViewController {
+    
+    @IBOutlet weak var nombreTitulo: UILabel!
+    
+    @IBOutlet weak var detalleImg: UIImageView!
+    
+    @IBOutlet weak var textoChef: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    
+    
+    
+}
