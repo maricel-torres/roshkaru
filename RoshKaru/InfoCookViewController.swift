16f802e836ab5566ed315ad7a61afc630bab2948
Arturo Fleitas <arturofleitasc@gmail.com>
2022-03-21 08:50:27 -0300

    storyboard info cooks

---
+++ RoshKaru/InfoCookViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoCookViewController.swift
+//  RoshKaru
+//
+//  Created by Bootcamp on 3/21/22.
+//  Copyright © 2022 Bootcamp. All rights reserved.
+//
+
+import UIKit
+
+class InfoCookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
