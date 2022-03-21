140ed433f9fa3d515ba2aa652ada779140a3c3b0
inicialComit <>
2022-03-21 15:32:19 -0300

    cambios menores en HechoViewController

--- RoshKaru/HechoViewController.swift
+++ RoshKaru/HechoViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class HechoViewController: UIViewController {
 
     var accessToken: String?
+    var carKey: String?
     override func viewDidLoad() {
         super.viewDidLoad()
 
