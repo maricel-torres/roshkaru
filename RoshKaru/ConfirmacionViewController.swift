41da0428bd4c97dfa068c3465e9a7ca8dab9f68d
inicialComit <>
2022-03-21 14:53:41 -0300

    cambios menores en ConfirmacionViewController

--- RoshKaru/ConfirmacionViewController.swift
+++ RoshKaru/ConfirmacionViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ConfirmacionViewController: UIViewController {
 
+    @IBOutlet weak var monto: UILabel!
+    @IBOutlet weak var pedidos: UITableViewCell!
     var accessToken: String?
+    var hud: MBProgressHUD?
     @IBOutlet weak var btnConfirm: MDCButton!
-    @IBOutlet weak var verticalStack: UIStackView!
     var carKey:String?
     var totalPagar:Int?
     var metodoPagoKey:String?
+    var BASEURL = "https://phoebe.roshka.com/eat"
+    @IBOutlet weak var total: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,11 @@ class ConfirmacionViewController: UIViewController {
         btnConfirm.backgroundColor = .systemRed
         btnConfirm.sizeToFit()
         btnConfirm.layer.cornerRadius = 18
+        
+        total.text = "Total:"
+        monto.text = "\(String(describing: totalPagar!)) gs"
+
+        
     }
     
 
@@ -33,9 +42,56 @@ class ConfirmacionViewController: UIViewController {
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "pedidoRealizado" {
-            self.performSegue(withIdentifier: "principal", sender: self.accessToken!)
+        if segue.identifier == "hecho" {
+            if let nexviewcontroller  = segue.destination as? HechoViewController {
+                nexviewcontroller.accessToken = self.accessToken
+                nexviewcontroller.carKey = self.carKey
+            }
         }
     }
 
+    func pay_cart(accessToken:String, cartKey: String?, paymentMethodKey: String?, total: Int?) {
+        var urlComponents = URLComponents(string: "\(BASEURL)/pay_cart")!
+        var queryItems: [URLQueryItem] = [
+            AccessTokenQueryItem(accessToken),
+            URLQueryItem(name: "cartKey", value: cartKey),
+            URLQueryItem(name: "paymentMethodKey", value: paymentMethodKey),
+            URLQueryItem(name: "total", value: String(total!))
+        ]
+        urlComponents.queryItems = queryItems
+        let url = urlComponents.url!
+        print(url.absoluteString)
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                self.hud?.hide(animated: true)
+            }
+            if let error = error {
+                print(error);
+            } else if let data = data {
+                let json = try? JSONSerialization.jsonObject(with: data, options: [.fragmentsAllowed])
+                if let json = json {
+                    print("\(String(data: try! JSONSerialization.data(withJSONObject: json, options: [.prettyPrinted]), encoding: .utf8)!)")
+                    
+                } else {
+                    self.successReally(data)
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "principal", sender: self.accessToken)
+                    }
+                }
+            }
+        }.resume()
+    }
+    
+    func AccessTokenQueryItem(_ at: String ) -> URLQueryItem {
+        URLQueryItem(name: "accessToken", value: at)
+    }
+    
+    func successReally(_ data: Data) {
+        if let str = String(data: data, encoding: .utf8), str.count > 0  {
+            print("_WOOOPS_______________________________________________\n\(str)")
+        } else {
+            print("# Success")
+        }
+    }
 }
